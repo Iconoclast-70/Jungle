d89341633a02b66951f5d1548edf6f573c9a86e1
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-07-22 04:18:21 +0000

    Admin dashboard product count and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,12 @@
+require 'pp'
+
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+
   def show
+    @products = Product.order(id: :desc).all
+    @categories = Category.all
+
   end
+
 end
