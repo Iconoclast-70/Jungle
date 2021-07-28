82d6c5e2ca068db73b47432f93d095921b2bd023
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-07-28 01:31:41 +0000

    Feature Spec: Product Details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
+require 'pp'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -17,16 +18,18 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "Can navigate from the home page to the product detail page by clicking on a product" do
     # ACT
     visit root_path
+    
+    click_link @category.products[0].name
 
     # DEBUG
     save_screenshot
+    puts page.html
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css 'div.col-sm-8'
   end
-  
-end
 
+end
